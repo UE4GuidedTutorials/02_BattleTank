ae7fd9ff353e9ea2a6e82f98c2149dd7213769bd
DESKTOP-8JGRS0R\Jon Kubera <jkuber1996@gmail.com>
2018-02-04 05:48:55 -0600

    Barrel's location is now the starting point of hit location

---
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameFramework/Actor.h"
+#include "UObject/UObjectGlobals.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankAimingComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLETANK_API UTankAimingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void AimAt(FVector HitLocation);
+
+	UTankAimingComponent();
+
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
+
+private:
+	UStaticMeshComponent* Barrel = nullptr;
+	
+};
